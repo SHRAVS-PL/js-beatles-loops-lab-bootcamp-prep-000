f9c62e04261a260f4b19fc9df31c1cdc0f462e69
Shravani PL <pl.shravani@gmail.com>
2018-06-02 00:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ function theBeatlesPlay(musicians,inst){
   for(var a = 0; a< musicians.length ;a++) {
      var str = musicians[a] + " plays " + inst[a]
         array.push(str)
-
-       }
-                  return array
+ }
+    return array
 
   }
