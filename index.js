395de527284e7a96e0a6e6bf8c45d846d0c25186
Shravani PL <pl.shravani@gmail.com>
2018-06-02 00:47:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians,inst){
-  var array = []
+  const array = []
   for(var a = 0; a< musicians.length ;a++) {
      var str = musicians[a] + " plays " + inst[a]
         array.push(str)
